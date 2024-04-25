2e8d9b706c9d7e7d162332a122979eb6e0b80496
gegabayte <xasanboymuhammaddinov41@gamil.com>
2024-04-25 14:43:50 +0500

    hatolar togilandi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,7 @@ function Header() {
                 <div className="w-3/5 mx-auto">
                     <div className="navbar ">
                         <div className="navbar-start">
-                            <Link to='/' className={`btn ${theme.theme == 'light' ? 'btn-info' : 'btn-secondary'} text-2xl hover:opacity-70`}>C</Link>
+                            <Link to='/' className={`btn ${theme.theme == 'light' ? 'btn-primary' : 'btn-secondary'} text-2xl hover:opacity-70`}>C</Link>
                         </div>
                         <div className="navbar-center hidden lg:flex">
                             <ul className="menu menu-horizontal px-1">
@@ -81,7 +81,7 @@ function Header() {
                                                 d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
                                             />
                                         </svg>
-                                        <span className={`badge badge-sm indicator-item ${theme.theme == 'light' ? 'badge-info' : 'badge-secondary'}`}>8</span>
+                                        <span className={`badge badge-sm indicator-item ${theme.theme == 'light' ? 'badge-primary' : 'badge-secondary'}`}>8</span>
                                     </div>
                                 </div>
                             </Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,63 +10,65 @@ function Home() {
   
     return (
       <div className='w-3/5 mx-auto mt-20'>
-        <main className='main flex items-center justify-between'>
-          <div className='info w-[496px]'>
-            <h1 className='max-w-2xl text-4xl font-bold tracking-tight sm:text-6xl text-[hsl(214, 30%, 32%)] mb-8'>
-              We are changing the way people shop
-            </h1>
-            <p className='text-lg leading-8 mb-8'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore
-              repellat explicabo enim soluta temporibus asperiores aut obcaecati
-              perferendis porro nobis.
-            </p>
-            <Link
-              to='/products'
-              className={`btn btn-${theme.theme == 'light' ? 'primary' : 'secondary'
-                } uppercase`}
-            >
-              our products
-            </Link>
-          </div>
-  
-          <div className='slider w-[464px]'>
-            <div className='carousel carousel-center max-w-md p-4 space-x-4 bg-neutral rounded-box '>
-              <div className='carousel-item'>
-                <img
-                  src={image1}
-                  className='rounded-box object-cover'
-                  width={320}
-                  height={416}
-  
-                />
-              </div>
-              <div className='carousel-item'>
-                <img
-                  src={image2}
-                  className='rounded-box object-cover'
-                  width={320}
-                  height={416}
-                />
-              </div>
-              <div className='carousel-item'>
-                <img
-                  src={image3}
-                  className='rounded-box object-cover'
-                  width={320}
-                  height={416}
-                />
-              </div>
-              <div className='carousel-item'>
-                <img
-                  src={image4}
-                  className='rounded-box object-cover'
-                  width={320}
-                  height={416}
-                />
-              </div>
+      <main className='main flex items-center justify-between'>
+        <div className='info w-[496px]'>
+          <h1 className='max-w-2xl text-4xl font-bold tracking-tight  sm:text-6xl text-[hsl(214, 30%, 32%)] mb-8'>
+            We are changing the way people shop
+          </h1>
+          <p className='text-lg leading-8 mb-8'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore
+            repellat explicabo enim soluta temporibus asperiores aut obcaecati
+            perferendis porro nobis.
+          </p>
+          <Link
+            to='/products'
+            className={`btn btn-${
+              theme.theme == 'light' ? 'primary' : 'secondary'
+            } uppercase`}
+          >
+            our products
+          </Link>
+        </div>
+
+        <div className='slider w-[464px]'>
+          <div className='carousel carousel-center  p-4 space-x-4 bg-neutral rounded-box'>
+            <div className='carousel-item'>
+              <img
+                src={image1}
+                className='rounded-box object-cover'
+                width={320}
+                height={416}
+
+              />
+            </div>
+            <div className='carousel-item'>
+              <img
+                src={image2}
+                className='rounded-box object-cover'
+                width={320}
+                height={416}
+              />
+            </div>
+            <div className='carousel-item'>
+              <img
+                src={image3}
+                className='rounded-box object-cover'
+                width={320}
+                height={416}
+              />
+            </div>
+            <div className='carousel-item'>
+              <img
+                src={image4}
+                className='rounded-box object-cover'
+                width={320}
+                height={416}
+              />
             </div>
           </div>
-        </main>
+        </div>
+      </main>
+
       </div>
     );
   }
